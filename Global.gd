01b747a798ce978de1fa333ee345c5c4ee9eef18
lu8dcf <lu8dcf@gmail.com>
2025-04-24 19:00:32 -0300

    Juego inicial solo 1 enemigo sin disparo

---
+++ Global.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+# Propiedades del  player 
+var lives = 3
+var speed_player = 500
+var mouse_sens = 0.5
+
+var speed_main = 200
+var deadzone_radius_main = 20 
+
+# Propiedades de la partida actual
+var score = 0
+var enemigos_eliminados = 0
+
+# Propiedades de la Pantalla y dispositivos
+var pantalla_ancho = 1280
+var pantalla_alto = 720
