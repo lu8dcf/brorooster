7a0bb3b5c9f596caceb33fd9a8f205c80d3df20b
FacundoMoure <facundo.moure@gmail.com>
2025-05-05 12:12:16 -0300

    armado de global de enemigo
    armado diccionario y enemigo 2

--- Global_enemy.gd
+++ Global_enemy.gd
@@ -1,1 +1,47 @@
-extends Node
+extends CharacterBody2D
+	
+class_name Enemy
+	
+var stats: Dictionary= {"nombre"= "", "vida"= 0, "alcance"= 0, "dano"= 0, "velocidad"= 0}
+var movimiento = Vector2()
+var velocidad = 0.5
+
+func _ready() -> void:
+	SetUp()
+	pass
+	
+func _physics_process(_delta): 
+	move_and_collide(movimiento)
+	if Global.lives >= 1:
+		set_vector(get_node("/root/main_game/player").global_position - global_position)
+	else:
+		movimiento = Vector2.ZERO
+	
+	
+func set_vector(vector):
+	movimiento = vector.normalized() * velocidad
+	if movimiento.x > 0:
+		$AnimationPlayer.play("right")
+	else:
+		$AnimationPlayer.play("left")
+	pass
+	
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		print ("toco")
+	pass # Replace with function body.
+	
+func SetUp():
+	pass
+	
+func Ataque():
+	
+	pass
+	
+func RecibirDano():
+	
+	pass
+	
+func Morir():
+	queue_free()
+	pass
--- scenes/game/enemy/enemy_2.gd
+++ scenes/game/enemy/enemy_2.gd
@@ -1,24 +1,18 @@
-extends CharacterBody2D
-
-var movimiento = Vector2()
-
-var velocidad = 0.5
-
-func _physics_process(delta: float) -> void:
-	move_and_collide(movimiento)
-	if Global.lives >= 1:
-		set_vector(get_node("/root/main_game/player").global_position - global_position)
-	else:
-		movimiento = Vector2.ZERO
-		
-func set_vector(vector):
-	movimiento = vector.normalized() * velocidad
+extends Enemy
+	
+func SetUp():
+	stats["nombre"] = "Gusano"
+	stats["vida"] = 5
+	stats["alcance"] = 0
+	stats["dano"] = 5
+	stats["velocidad"] = 1
 	pass
 	
-
-func _on_area_2d_body_entered(body):
+func UsarStats(enemyGlobal: Dictionary):
+	stats["nombre"] += enemyGlobal["nombre"]
+	stats["vida"] += enemyGlobal["vida"]
+	stats["alcance"] += enemyGlobal["alcance"]
+	stats["dano"] += enemyGlobal["dano"]
+	stats["velocidad"] += enemyGlobal["velocidad"]
+	pass
 	
-	if body.is_in_group("player"):
-		
-		print ("toco")
-	pass # Replace with function body.
