78b2dd2ee76e2cbc7be899aeb38749c683b68c1e
Heber Alonso <lu8dcf@gmail.com>
2025-05-15 22:32:51 -0300

    Agregado del disparoi generico desde armas, con seguimiento funcional 15/05

--- scripts/game/laser.gd
+++ scripts/game/laser.gd
@@ -2,13 +2,23 @@ extends Area2D
 
 var speed = 600.0  # Velocidad del láser
 var tiempo_sonido = .2
+var damage = 10 # Daño que genera la bala, esto se debera hacer generico
+var direction: Vector2 # direccion hacia donde ira la bala, enemigo mas cercano
 
 func _ready():
-	$laser.play() # Sonido del laser a disparar
+	$sonido_disparo.play() # Sonido del laser a disparar
 	
 	
 func _physics_process(delta):
-	position.y -= speed * delta # Mover el láser hacia arriba
+	position += direction * speed  * delta # Mover el disparo
 	# Si el láser sale de la pantalla, se elimina
-	if position.y < 0:
+	if position.y < 0 or position.x < 0:  # falta agregar los exptremos para cuando la bala salga de pantalla
 		queue_free()
+
+func set_direction(dir: Vector2): # Direccion de la bala
+	direction = dir
+	
+func _on_body_entered(body):   # cuando pege en el enemigo el daño que le provoca
+	if body.is_in_group("enemies"):
+		body.take_damage(damage)  # Método en el enemigo
+	queue_free()
