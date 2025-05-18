1cd98a2baa4b44862efadb8492faeeca8462bf8b
Luxa <luxamgames2000@gmail.com>
2025-05-18 18:27:57 -0300

    correccion de codigo en menu de pausa
    correccion de error en menu de pausa al tocar el ESC por segunda vez. mejora de codigo y limpieza de duplicados

--- Global_hud.gd
+++ Global_hud.gd
@@ -21,20 +21,20 @@ func _on_exit_pressed() -> void:
 	get_tree().quit()
 
 #TECLA ESCAPE PARA LA PAUSA
-func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("ui_esc"): # al presionarse la tecla ESC configurada en el proyecto el juego pasará a estar en el estado de pausa
-		# de la siguiete forma se detiene el juego y si este código se recnoce que ya está en pausa, con la misma tecla se puede regresar al juego
-		
-		if get_tree().paused == true :
-			$blur_pausa/animator.play("resume_pause_blur")
-			$blur_pausa/bg_overlay/sound_pause_off.play()
-		else:
-			$blur_pausa/animator.play("pause_blur")
-			$blur_pausa/bg_overlay/sound_pause_on.play()
-			
-		#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		get_tree().paused = not get_tree().paused
-		
-		$blur_pausa/bg_overlay/VBoxContainer.visible = not $blur_pausa/bg_overlay/VBoxContainer.visible
-		#$VBoxContainer.visible = not $VBoxContainer.visible
-		#$Popup.visible = not $Popup.visible
+#func _input(_event: InputEvent) -> void:
+	#if Input.is_action_just_pressed("ui_esc"): # al presionarse la tecla ESC configurada en el proyecto el juego pasará a estar en el estado de pausa
+		## de la siguiete forma se detiene el juego y si este código se recnoce que ya está en pausa, con la misma tecla se puede regresar al juego
+		#
+		#if get_tree().paused == true :
+			#$blur_pausa/animator.play("resume_pause_blur")
+			#$blur_pausa/bg_overlay/sound_pause_off.play()
+		#else:
+			#$blur_pausa/animator.play("pause_blur")
+			#$blur_pausa/bg_overlay/sound_pause_on.play()
+			#
+		##Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		#get_tree().paused = not get_tree().paused
+		#
+		#$blur_pausa/bg_overlay/VBoxContainer.visible = not $blur_pausa/bg_overlay/VBoxContainer.visible
+		##$VBoxContainer.visible = not $VBoxContainer.visible
+		##$Popup.visible = not $Popup.visible
