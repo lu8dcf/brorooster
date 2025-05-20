e92a64666334007feb8a288739b03fbaa29673f0
Heber Alonso <lu8dcf@gmail.com>
2025-05-20 09:01:03 -0300

    Cambio de seleecion de player desde el spritr

--- scripts/game/player/playerFactory.gd
+++ scripts/game/player/playerFactory.gd
@@ -1,6 +1,6 @@
 class_name PlayerFactory
 
-static func load_player(scene_path: String, health: int, speed: int, armor: int, sprite_path: NodePath) -> Player:
+static func load_player(scene_path: String, health: int, speed: int, armor: int, sprite_path: String) -> Player:
 	var player_scene = load(scene_path) as PackedScene
 	if player_scene:
 		var player = player_scene.instantiate() as Player
