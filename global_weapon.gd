a42a22ce1df1fcd29e3afb0f97edae135e00b7c7
Violeta <violemf@gmail.com>
2025-06-02 18:39:35 -0300

    Agregue en GlobalWeapon Cadencia y danio
    Permite modificar el tiempo entre disparo y el danio de las armas en ejecucion. (se debe modificar el script GlobalWeapon o settear).
    Lo siguiente es voy a rotar las armas para que no queden torcidas.

--- global_weapon.gd
+++ global_weapon.gd
@@ -1,19 +1,27 @@
-extends Resource
-class_name WeaponData
+extends Node
 
-# Propiedades exportadas para configurar en el editor
-@export var _id: int = 0
-@export var _display_name: String = "Arma"
-@export var _weapon_type: String = "Ametralladora"
-@export var _texture: Texture2D
-@export var _sprite_texture: String = "res://assets/graphics/character_graphics/armas/arma1.png" # texto de la ubicacion exacta del sprite
-@export var _cost: float = 1
-@export var _shoot_time: float = 1
-@export var _bullet_type: String = "A"
+@export var CadenciaDisparo :  float = 1.0 #Recordar que hay velocidades de por ejemplo 0,2
+#Por lo tando 15.0 * 0.2 da 3, 3 segundos entre disparo. (15.0 es excelente para probar)
+@export var danio :int = 1
 
-@export var _rarety: int = 0:   # comun = 0, rara = 1, épica = 2 y legendaria = 3
-	set(value):
-		_rarety = clamp(value, 0, 4)
+
+
+#set
+func set_CadenciaGlobal (valor: float):
+	CadenciaDisparo = valor
+	pass
+func set_danioGlobal(valor: int):
+	danio = valor
+	pass
+	
+#getter
+func get_CadenciaGlobal() -> float:
+	return CadenciaDisparo
+	pass
+	
+func get_danioGlobal() -> int:
+	return danio
+	pass
 
 # Señales
 signal weapon_updated
@@ -26,10 +34,13 @@ func buy(amount: float) -> void:
 	pass
 func sell(amount: float) -> void:
 	pass
-
-# getters
-func get_cost() -> float:
-	return _cost
-	
-func get_rarety() -> float:
-	return _rarety
+ 
+#Ey, con esto se puede proponer algo interesante
+#func _ready():
+	#match dificultad_actual:
+		#"fácil":
+			#GlobalWeapon.daño_base_global = 15
+			#GlobalWeapon.cadencia_base_global = 0.4
+		#"difícil":
+			#GlobalWeapon.daño_base_global = 8
+			#GlobalWeapon.cadencia_base_global = 0.7
