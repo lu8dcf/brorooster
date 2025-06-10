2eb9c0f2607ca965932df2aca174daffacf87692
Heber Alonso <lu8dcf@gmail.com>
2025-06-10 13:10:34 -0300

    todos.bat

---
+++ todos.bat
@@ -0,0 +1,15 @@
+git checkout menu
+git merge main
+git push
+git checkout gdd
+git merge main
+git push
+git checkout efectos
+git merge main
+git push
+git checkout grafico
+git merge main
+git push
+git checkout juego
+git merge main
+git push
