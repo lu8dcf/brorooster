1846a21dc555c2a486d492ea7ee8ff0a9b2e1c1e
Heber Alonso <lu8dcf@gmail.com>
2025-06-10 19:56:09 -0300

    enemigo 3 bolita, rueda y tiene su forma de moverse

--- scripts/game/enemy/enemy1.gd
+++ scripts/game/enemy/enemy1.gd
@@ -5,10 +5,8 @@ extends EnemigoBase
 # Movimiento exclusivo de la Babosa
 func _physics_process(_delta):
 	move_and_collide(movimiento)
-	if Global.currentPlayer._health >= 1:
-		set_vector(get_node("/root/main_game/player").global_position - global_position)
-	else:
-		movimiento = Vector2.ZERO
+	set_vector(get_node("/root/main_game/player").global_position - global_position)
+	
 
 func set_vector(vector):
 	movimiento = vector.normalized() * veloci
