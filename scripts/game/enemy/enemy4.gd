ab24cbf5c243ffd7cfca288971e9888297d8c0e4
Heber Alonso <lu8dcf@gmail.com>
2025-06-10 18:08:24 -0300

    enemy 3

--- scripts/game/enemy/enemy4.gd
+++ scripts/game/enemy/enemy4.gd
@@ -4,15 +4,15 @@ class_name Langosta
 
 # Lógica de salto
 var salto_timer := Timer.new()
-var salto_fuerza := 10.0
-var desaceleracion := 0.9
+#var salto_fuerza := 10.0
+#var desaceleracion := 0.9
 
 func _ready():
 	$Sprite2D.texture = load(sprite)
 	$Sprite2D.modulate = Color(red, green, blue)  # Dejado igual que tu código original
 	cpu_particles = get_node("CPUParticles2D")  # o $CPUParticles2D
 	# Configurar el timer de salto
-	salto_timer.wait_time = 1.5
+	salto_timer.wait_time = 2
 	salto_timer.one_shot = false
 	salto_timer.connect("timeout", Callable(self, "_on_salto_timer_timeout"))
 	add_child(salto_timer)
@@ -20,12 +20,12 @@ func _ready():
 
 func _physics_process(_delta): 
 	move_and_collide(movimiento)
-	movimiento *= desaceleracion  # Frena el movimiento tras el salto
+	movimiento = Vector2(0.0,0.0) # Frena el movimiento tras el salto
 
 func _on_salto_timer_timeout():
 	recibe_danio=false
 	var direccion = get_node("/root/main_game/player").global_position - global_position
-	movimiento = direccion.normalized() * salto_fuerza
+	movimiento = direccion.normalized() * veloci
 	if movimiento.x > 0:
 		$AnimationPlayer.play("right")
 	else:
