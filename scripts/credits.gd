a8a3b200a8b858de5150623c8eb500722e4dea85
RAX0909 <ramiroelian2003@gmail.com>
2025-06-13 18:22:31 -0300

    creditos aun falta que se puedan sacar

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -1,4 +1,9 @@
 extends Control
 
+
+@onready var  animation = $AnimationPlayer
 func _ready() -> void:
-	pass
+	# Cargar y reproducir el sonido
+	var sound = load("res://assets/sound/menus_effects/muerte.mp3")
+
+	animation.play("Creditos")
